f4fef089d8a28d8761333e3ef699e0874ab6c0de
BradyBlackley <bradyblackley@mail.weber.edu>
2021-02-09 10:53:07 -0700

    added .idea file to git repo to avoid having to reconfigure project in intellij

--- scripts/magic_db_tables_script.sql
+++ scripts/magic_db_tables_script.sql
@@ -36,11 +36,12 @@ CREATE TABLE expansion (
 );
 
 CREATE TABLE card (
-	card_id int NOT NULL AUTO_INCREMENT,
+	card_id varchar(255) NOT NULL,
     name varchar(255),
     typeline_id int,
     mana_type ENUM('white', 'blue', 'black', 'red', 'green', 'colorless', 'multi-colored'),
-    image BLOB,
+    image_path varchar(255),
+    rarity varchar(255),
     artist_name varchar(255),
     converted_mana_cost int,
     power int,
@@ -48,6 +49,8 @@ CREATE TABLE card (
     keyword_list_id int,
     expansion_id int,
     collector_number int,
+    is_modal bool,
+    is_alternate_art bool,
     PRIMARY KEY (card_id),
     FOREIGN KEY (typeline_id) REFERENCES typeline(typeline_id),
     FOREIGN KEY (keyword_list_id) REFERENCES keyword_list(keyword_list_id),
@@ -56,7 +59,42 @@ CREATE TABLE card (
 
 CREATE TABLE card_collection (
 	card_collection_id int NOT NULL AUTO_INCREMENT,
-    card_id int,
+    card_id varchar(255),
     PRIMARY KEY (card_collection_id),
     FOREIGN KEY (card_id) REFERENCES card(card_id)
+);
+
+CREATE TABLE user (
+	user_id int NOT NULL AUTO_INCREMENT,
+    card_collection_id int,
+    password varchar(255),
+    user_name varchar(255),
+    PRIMARY KEY (user_id),
+    FOREIGN KEY (card_collection_id) REFERENCES card_collection(card_collection_id)
+);
+
+CREATE TABLE modal (
+	modal_id int NOT NULL AUTO_INCREMENT,
+    front_card_id varchar(255),
+    back_card_id varchar(255),
+    PRIMARY KEY (modal_id),
+    FOREIGN KEY (front_card_id) REFERENCES card(card_id),
+    FOREIGN KEY (back_card_id) REFERENCES card(card_id)
+);
+
+CREATE TABLE card_copy (
+	card_copy_id int NOT NULL AUTO_INCREMENT,
+    card_id varchar(255),
+    PRIMARY KEY (card_copy_id),
+    FOREIGN KEY (card_id) REFERENCES card(card_id)
+);
+
+CREATE TABLE library (
+	library_id int NOT NULL AUTO_INCREMENT,
+    library_name varchar(255),
+    card_collection_id int,
+    card_copy_id int,
+    PRIMARY KEY (library_id),
+    FOREIGN KEY (card_collection_id) REFERENCES card_collection(card_collection_id),
+    FOREIGN KEY (card_copy_id) REFERENCES card_copy(card_copy_id)
 );
