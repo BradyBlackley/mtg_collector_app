75c71eb692b349ceb9d9cd5fe45b9b73fc7c194a
BradyBlackley <bradyblackley@mail.weber.edu>
2022-02-18 14:01:12 -0600

    added userjdbc repoistory

--- mtg/src/main/java/com/example/mtg/repository/UserJdbcRepository.java
+++ mtg/src/main/java/com/example/mtg/repository/UserJdbcRepository.java
@@ -1,39 +1,81 @@
 package com.example.mtg.repository;
 
 import com.example.mtg.model.User;
+import com.example.mtg.repository.mappers.UserMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public class UserJdbcRepository implements UserRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public UserJdbcRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public List<User> findAll() {
-        return null;
+        final String sql = "select user_id, username, `password` from `user`;";
+        return jdbcTemplate.query(sql, new UserMapper());
     }
 
     @Override
     public User findByUsername(String username) {
-        return null;
+        final String sql = "select user_id, username, `password` from `user`" +
+                "where username = ?;";
+        return jdbcTemplate.query(sql, new UserMapper(), username).stream().findFirst().orElse(null);
     }
 
     @Override
     public User findById(String userId) {
-        return null;
+        final String sql = "select user_id, username, `password` from `user`" +
+                "where user_id = ?;";
+        return jdbcTemplate.query(sql, new UserMapper(), userId).stream().findFirst().orElse(null);
     }
 
     @Override
     public User add(User user) {
-        return null;
+
+        user.setUserId(java.util.UUID.randomUUID().toString());
+
+        final String sql = "insert into `user` (user_id, username, `password`)" +
+                " values (?,?,?);";
+
+        int rowsAffected = jdbcTemplate.update(sql,
+                user.getUserId(),
+                user.getUsername(),
+                user.getPassword());
+
+        if (rowsAffected <= 0) {
+            return null;
+        }
+
+        return user;
     }
 
     @Override
     public boolean update(User user) {
-        return false;
+
+        final String sql = "update user set " +
+                "username = ?, " +
+                "`password` = ? " +
+                "where user_id = ?;";
+
+        return jdbcTemplate.update(sql,
+                user.getUsername(),
+                user.getPassword(),
+                user.getUserId()) > 0;
     }
 
     @Override
     public boolean deleteById(String userId) {
-        return false;
+
+        final String sql = "set sql_safe_updates = 0; " +
+                "delete from `user` where user_id = ?; " +
+                "set sql_safe_updates = 1;";
+        return jdbcTemplate.update(sql, userId) > 0;
     }
 }
