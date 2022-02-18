2fefec2d29ad754f0e9a16630e81385da3a77de6
BradyBlackley <bradyblackley@mail.weber.edu>
2022-02-18 14:43:39 -0600

    added cool card count to home page

--- view/src/components/Home.js
+++ view/src/components/Home.js
@@ -1,11 +1,18 @@
-const divStyle = {
+const homeStyle = {
+
+}
+
+const homeTitleStyle = {
     textAlign: "center",
     marginTop: "5px"
 }
 
 function Home(props) {
     return(
-        <div className="home" style={divStyle}><h3>Welcome {props.username}</h3></div>
+        <div className="homeContainer" style={homeStyle}>
+            <div className="homeTitle" style={homeTitleStyle}><h3>Welcome {props.username}</h3></div>
+            <div className="cardCount">You own [ ] total cards</div>
+        </div>
     )
 }
 
