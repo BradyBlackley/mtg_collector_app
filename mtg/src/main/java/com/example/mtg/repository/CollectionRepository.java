1c48de21f659ab7b61468b368d09086cb6bea8f0
BradyBlackley <bradyblackley@mail.weber.edu>
2022-02-22 14:59:35 -0600

    added collection repository

--- mtg/src/main/java/com/example/mtg/repository/CollectionRepository.java
+++ mtg/src/main/java/com/example/mtg/repository/CollectionRepository.java
@@ -1,2 +1,20 @@
-package com.example.mtg.repository;public interface Collection {
+package com.example.mtg.repository;
+
+import com.example.mtg.model.Collection;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public interface CollectionRepository {
+    List<Collection> findAll();
+
+    Collection findById(int collectionId);
+
+    Collection add(Collection collection);
+
+    boolean update(Collection collection);
+
+    @Transactional
+    boolean deleteById(String collectionId);
+
 }
