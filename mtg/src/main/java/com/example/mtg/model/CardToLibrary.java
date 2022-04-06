9a0bbf59422a2000efbf50ec3a512c3c15a6dd78
BradyBlackley <bradyblackley@mail.weber.edu>
2022-04-06 17:38:31 -0500

    added card to library model

--- mtg/src/main/java/com/example/mtg/model/CardToLibrary.java
+++ mtg/src/main/java/com/example/mtg/model/CardToLibrary.java
@@ -1,2 +1,41 @@
-package com.example.mtg.model;public class CardToLibrary {
+package com.example.mtg.model;
+
+import java.util.List;
+
+public class CardToLibrary {
+
+    private int cardToLibrary;
+    private List<CardCopy> cardCopies;
+    private Library library;
+
+
+    public CardToLibrary(int cardToLibrary, List<CardCopy> cardCopies, Library library) {
+        this.cardToLibrary = cardToLibrary;
+        this.cardCopies = cardCopies;
+        this.library = library;
+    }
+
+    public int getCardToLibrary() {
+        return cardToLibrary;
+    }
+
+    public void setCardToLibrary(int cardToLibrary) {
+        this.cardToLibrary = cardToLibrary;
+    }
+
+    public List<CardCopy> getCardCopies() {
+        return cardCopies;
+    }
+
+    public void setCardCopies(List<CardCopy> cardCopies) {
+        this.cardCopies = cardCopies;
+    }
+
+    public Library getLibrary() {
+        return library;
+    }
+
+    public void setLibrary(Library library) {
+        this.library = library;
+    }
 }
