7f9b9b222b691f1d318887cf2c1b98069afa8eb0
BradyBlackley <bradyblackley@mail.weber.edu>
2022-04-06 18:28:16 -0500

    added color rowmapper

--- mtg/src/main/java/com/example/mtg/repository/mappers/ColorMapper.java
+++ mtg/src/main/java/com/example/mtg/repository/mappers/ColorMapper.java
@@ -1,2 +1,18 @@
-package com.example.mtg.repository.mappers;public class ColorMapper {
+package com.example.mtg.repository.mappers;
+
+import com.example.mtg.model.Color;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ColorMapper implements RowMapper<Color> {
+
+    @Override
+    public Color mapRow(ResultSet rs, int i) throws SQLException {
+        Color color = new Color();
+        color.setColorId(rs.getInt("color_id"));
+        color.setColorName(rs.getString("color_name"));
+        return color;
+    }
 }
