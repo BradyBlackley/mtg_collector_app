fb7838cad43ee5084869f530fb76a816936a9192
BradyBlackley <bradyblackley@mail.weber.edu>
2022-04-06 18:20:28 -0500

    added color repository interface

---
+++ mtg/src/main/java/com/example/mtg/repository/ColorRepository.java
@@ -0,0 +1,21 @@
+package com.example.mtg.repository;
+
+import com.example.mtg.model.Color;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public interface ColorRepository {
+    List<Color> findAllColors();
+
+    Color findColorById(int colorId);
+
+    Color findColorByName(String colorName);
+
+    Color add(Color color);
+
+    boolean update(Color color);
+
+    @Transactional
+    boolean deleteByName(int colorName);
+}
