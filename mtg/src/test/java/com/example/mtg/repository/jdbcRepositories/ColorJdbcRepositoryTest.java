cbe1a41aedbd997abe279aa9baaee35654e401c6
BradyBlackley <bradyblackley@mail.weber.edu>
2022-05-23 11:15:07 -0500

    add color jdbc repository test

---
+++ mtg/src/test/java/com/example/mtg/repository/jdbcRepositories/ColorJdbcRepositoryTest.java
@@ -0,0 +1,32 @@
+package com.example.mtg.repository.jdbcRepositories;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ColorJdbcRepositoryTest {
+
+    @Test
+    void findAllColors() {
+    }
+
+    @Test
+    void findColorById() {
+    }
+
+    @Test
+    void findColorByName() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void deleteByName() {
+    }
+}
