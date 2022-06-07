c8d3dad4e2a150ed3f40af6f963ae5d10066e469
BradyBlackley <bradyblackley@mail.weber.edu>
2022-06-07 13:53:25 -0500

    added updates to readme and dev,prod application properties

--- mtg/README.md
+++ mtg/README.md
@@ -1,4 +1,4 @@
 Two profiles: dev, prod
-In run configurations add -Dspring.profiles.active=dev to activate
+In run configurations add -Dspring.profiles.active=dev to VM options to activate
 
 Set two environment variables {USERNAME}, {PASSWORD}
--- mtg/src/test/java/com/example/mtg/repository/jdbcRepositories/ExpansionJdbcRepositoryTest.java
+++ mtg/src/test/java/com/example/mtg/repository/jdbcRepositories/ExpansionJdbcRepositoryTest.java
@@ -1,10 +1,21 @@
 package com.example.mtg.repository.jdbcRepositories;
 
+import com.example.mtg.repository.CommonRepoTest;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ExpansionJdbcRepositoryTest {
+@Sql({"classpath:data/Expansion.sql"})
+@ContextConfiguration(
+        classes = {ExpansionJdbcRepository.class}
+)
+class ExpansionJdbcRepositoryTest extends CommonRepoTest {
+
+    @Autowired
+    ExpansionJdbcRepository repository;
 
     @Test
     void findAllExpansions() {
