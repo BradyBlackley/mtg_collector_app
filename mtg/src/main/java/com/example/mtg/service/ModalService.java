6fbac5d0f29b9e4c80aad59dcd59c5ff566569ae
BradyBlackley <bradyblackley@mail.weber.edu>
2022-11-28 20:26:37 -0600

    added modal add tests

--- mtg/src/main/java/com/example/mtg/service/ModalService.java
+++ mtg/src/main/java/com/example/mtg/service/ModalService.java
@@ -7,6 +7,9 @@ import com.example.mtg.service.result.ResultType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Service
 public class ModalService {
     @Autowired
@@ -23,6 +26,70 @@ public class ModalService {
             result.addMessage(ResultType.SUCCESS.label, ResultType.SUCCESS);
         }
         return result;
+    }
+
+    public Result<Modal> findByBackCardId(String cardId) {
+        Result<Modal> result = new Result<>();
+        result.setPayload(repository.findByBackCardId(cardId));
+        if(result.getPayload() == null) {
+            result.addMessage("No cards found by given back card id " + cardId, ResultType.NOT_FOUND);
+        } else {
+            result.addMessage(ResultType.SUCCESS.label, ResultType.SUCCESS);
+        }
+        return result;
+    }
+
+    public Result<Modal> findByModalId(String cardId) {
+        Result<Modal> result = new Result<>();
+        result.setPayload(repository.findByModalId(cardId));
+        if(result.getPayload() == null) {
+            result.addMessage("No cards found by given modal id " + cardId, ResultType.NOT_FOUND);
+        } else {
+            result.addMessage(ResultType.SUCCESS.label, ResultType.SUCCESS);
+        }
+        return result;
+    }
+
+    public Result<Modal> add(Modal modal) {
+        Result<Modal> result = new Result<>();
+
+        if(!validateModal(modal)) {
+            result.addMessage("The given modal " + modal + " is invalid", ResultType.INVALID);
+        } else if (repository.findByModalId(modal.getModalId()) != null) {
+            result.addMessage("The given modal " + modal + " already exists", ResultType.INVALID);
+        } else {
+            result.setPayload(repository.add(modal));
+            if(result.getPayload() == null) {
+                result.addMessage("Failed to add given modal " + modal, ResultType.ERROR);
+            } else {
+                result.addMessage(ResultType.SUCCESS.label, ResultType.SUCCESS);
+            }
+        }
+        return result;
+    }
+
+    public boolean validateModal(Modal modal) {
+        return validateModalId(modal.getModalId())
+                && validateFrontCardId(modal.getfrontCard().getCardId())
+                && validateBackCardId(modal.getbackCard().getCardId());
+    }
+
+    private boolean validateModalId(String modalId) {
+        Pattern pattern = Pattern.compile("^[A-Z\\d]{3}\\d{3}$");
+        Matcher matcher = pattern.matcher(modalId);
+        return matcher.matches();
+    }
+
+    private boolean validateFrontCardId(String cardId) {
+        Pattern pattern = Pattern.compile("^[A-Z\\d]{3}\\d{3}[FB]$");
+        Matcher matcher = pattern.matcher(cardId);
+        return matcher.matches();
+    }
+
+    private boolean validateBackCardId(String cardId) {
+        Pattern pattern = Pattern.compile("^[A-Z\\d]{3}\\d{3}[FB]$");
+        Matcher matcher = pattern.matcher(cardId);
+        return matcher.matches();
     }
 
 }
--- mtg/src/test/java/com/example/mtg/service/ModalServiceTest.java
+++ mtg/src/test/java/com/example/mtg/service/ModalServiceTest.java
@@ -41,7 +41,23 @@ class ModalServiceTest {
             "As Turntimber, Serpentine Wood enters the battlefield, you may pay 3 life. If you don't, it enters " +
                     "the battlefield tapped. [tap]: Add [green]");
 
+    public Card invalidFrontCard = new Card("ZNR215", "Turntimber Symbiosis",
+            "card_images\\zendikar_rising\\znr-215-turntimber-symbiosis.jpg", Rarity.MYTHIC,
+            "Randy Vargas", "7", null, null, new Expansion(1,
+            "Zendikar Rising", "ZNR", Date.valueOf("2020-09-01")),
+            "Look at the top seven cards of your library. You may put a creature card from among them into the" +
+                    " battlefield. If that card has a converted mana cost 3 or less, it enters with three additional " +
+                    "+1/+1 counters on it. Put the rest on the bottom of your library in a random order.");
+
+    public Card invalidBackCard = new Card("ZNR215", "Turntimber, Serpentine Wood",
+            "card_images\\zendikar_rising\\znr-215-turntimber-serpentine-wood.jpg", Rarity.MYTHIC,
+            "Randy Vargas", null, null, null, new Expansion(1,
+            "Zendikar Rising", "ZNR", Date.valueOf("2020-09-01")),
+            "As Turntimber, Serpentine Wood enters the battlefield, you may pay 3 life. If you don't, it enters " +
+                    "the battlefield tapped. [tap]: Add [green]");
+
     public Modal validModal = new Modal("ZNR215", validFrontCard, validBackCard);
+    public Modal invalidModal = new Modal("B@d", invalidFrontCard, invalidBackCard);
 
     @Test
     void findByFrontCardId() {
@@ -56,5 +72,66 @@ class ModalServiceTest {
         assertFalse(service.findByFrontCardId(validFrontCard.getCardId()).isSuccess());
         assertEquals("No cards found by given front card id " + validFrontCard.getCardId(),
                 service.findByFrontCardId(validFrontCard.getCardId()).getMessages().get(0));
+    }
+
+    @Test
+    void findByBackCardId() {
+        Mockito.when(repository.findByBackCardId(validFrontCard.getCardId())).thenReturn(validModal);
+        assertTrue(service.findByBackCardId(validFrontCard.getCardId()).isSuccess());
+        assertEquals("success", service.findByBackCardId(validFrontCard.getCardId()).getMessages().get(0));
+    }
+
+    @Test
+    void findByBackCardId_NoneFound() {
+        Mockito.when(repository.findByBackCardId(validFrontCard.getCardId())).thenReturn(null);
+        assertFalse(service.findByBackCardId(validFrontCard.getCardId()).isSuccess());
+        assertEquals("No cards found by given back card id " + validFrontCard.getCardId(),
+                service.findByBackCardId(validFrontCard.getCardId()).getMessages().get(0));
+    }
+
+    @Test
+    void findByModalId() {
+        Mockito.when(repository.findByModalId(validFrontCard.getCardId())).thenReturn(validModal);
+        assertTrue(service.findByModalId(validFrontCard.getCardId()).isSuccess());
+        assertEquals("success", service.findByModalId(validFrontCard.getCardId()).getMessages().get(0));
+    }
+
+    @Test
+    void findByModalId_NoneFound() {
+        Mockito.when(repository.findByModalId(validFrontCard.getCardId())).thenReturn(null);
+        assertFalse(service.findByModalId(validFrontCard.getCardId()).isSuccess());
+        assertEquals("No cards found by given modal id " + validFrontCard.getCardId(),
+                service.findByModalId(validFrontCard.getCardId()).getMessages().get(0));
+    }
+
+    @Test
+    void add() {
+        Mockito.when(repository.add(validModal)).thenReturn(validModal);
+        assertTrue(service.add(validModal).isSuccess());
+        assertEquals("success", service.add(validModal).getMessages().get(0));
+    }
+
+    @Test
+    void add_InvalidModal() {
+        assertFalse(service.add(invalidModal).isSuccess());
+        assertEquals("The given modal " + invalidModal + " is invalid",
+                service.add(invalidModal).getMessages().get(0));
+    }
+
+    @Test
+    void add_ModalAlreadyExists() {
+        Mockito.when(repository.findByModalId(validModal.getModalId())).thenReturn(validModal);
+        assertFalse(service.add(validModal).isSuccess());
+        assertEquals("The given modal " + validModal + " already exists",
+                service.add(validModal).getMessages().get(0));
+    }
+
+    @Test
+    void add_Failed() {
+        Mockito.when(repository.findByModalId(validModal.getModalId())).thenReturn(validModal);
+        Mockito.when(service.add(validModal)).thenReturn(null);
+        assertFalse(service.add(validModal).isSuccess());
+        assertEquals("Failed to add given modal " + validModal,
+                service.add(validModal).getMessages().get(0));
     }
 }
