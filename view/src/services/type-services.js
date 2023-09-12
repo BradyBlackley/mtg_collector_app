22352d1a537bc2e48cb22197b40d1d43638a724d
BradyBlackley <bradyblackley@mail.weber.edu>
2023-09-12 11:34:53 -0600

    added WOE cards, updated type-services in view

--- view/src/services/type-services.js
+++ view/src/services/type-services.js
@@ -17,3 +17,22 @@ export async function getAllTypes(){
     const {messages} = await response.json();
     return Promise.reject(messages);
 }
+
+export async function addType(type){
+    const init = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(type)
+    };
+
+    const response =  await fetch(`${url}/addType`, init);
+    if(response.status === 200){
+        return response;
+    }
+
+    const {messages} = await response.json();
+    return Promise.reject(messages);
+}
