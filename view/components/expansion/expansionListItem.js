bea6d5a1908318959f178e316812ee044d1c81b6
BradyBlackley <bradyrblackley@gmail.com>
2024-02-15 16:34:55 -0700

    added date formatter for released date

--- view/components/expansion/expansionListItem.js
+++ view/components/expansion/expansionListItem.js
@@ -1,10 +1,12 @@
+import dateFormatter from "@/helpers/dateFormatter";
+
 const ExpansionListItem = ({ expansionId, expansionName, expansionCode, releasedDate }) => {
     return(
         <tr>
             <td>{expansionId}</td>
             <td>{expansionName}</td>
             <td>{expansionCode}</td>
-            <td>{releasedDate}</td>
+            <td>{dateFormatter.format(Date.parse(releasedDate))}</td>
         </tr>
     );
 };
